68cbc5d778d6c66a6476cdc2ef22bf3c00583d60
Swiss <69964126+ogechibogu@users.noreply.github.com>
2022-02-19 05:18:29 +0100

    Add files via upload
    First Print commit

---
+++ print.py
@@ -0,0 +1,13 @@
+#I BUILT MY FIRST CHATBOT 
+
+
+
+print('Hello\nWelcome to KAUST')
+
+name=input('What is your name?\n')
+menu=('Masters, PhD, Post Doc')
+print('Nice to meet you '+ name +'\n' 'At KAUST we offer '+ menu)
+
+package=input('what would you like?\n')
+
+print(package + ' It is!\n' 'Enjoy your stay' )
